8d456a59759556907fa58fe0c977d31d824dae77
shunyata <empty@cqdr.es>
2013-02-26 21:02:33 -0500

    Added prototype image view page HTML stuff and a route for it.

--- booru/views.py
+++ booru/views.py
@@ -18,4 +18,7 @@ def upload():
     return render_template("booru/upload.html")
 
 
+@mod.route('/i/<image_id>')
+def image(image_id):
+    return render_template("booru/image-view.html", **locals())
 
