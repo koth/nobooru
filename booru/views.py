d902eb5faffe42af4bf3f1ef77a9ae56b5f20c6c
shunyata <empty@cqdr.es>
2013-02-26 23:01:55 -0500

    Added stub models for fakey templates.

--- booru/views.py
+++ booru/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+import datetime
+from booru.models import Image, Artist
 from flask import (
     Blueprint,
     render_template,
 )
 from flask.ext.login import current_user
+from users.models import User
 
 mod = Blueprint('booru', __name__)
 
@@ -20,5 +23,26 @@ def upload():
 
 @mod.route('/i/<image_id>')
 def image(image_id):
-    return render_template("booru/image.html", **locals())
+    image = Image(
+        filename="http://placekitten.com/800/600",
+        description="What a wonderfoo world.",
+        file_hash="foo",
+        upload_time=datetime.datetime(year=2013, month=2, day=12, hour=13, minute=37),
+        width=800,
+        height=600,
+        source_url="http://deviantart.com/whathuh",
+    )
+
+    artist = Artist(
+        name="KatPainter",
+    )
+
+    uploader = User(
+        name="Horsefly",
+        email="foo@foo.com",
+        password="foo",
+    )
+
+
+    return render_template("booru/image.html", image=image, artist=artist, uploader=uploader)
 
