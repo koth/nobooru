5d1abc29c8f4be70f20cf1733e095345702540e0
shunyata <empty@cqdr.es>
2013-02-27 13:17:35 -0500

    Image view gets real data from DB

--- booru/views.py
+++ booru/views.py
@@ -21,28 +21,11 @@ def upload():
     return render_template("booru/upload.html")
 
 
-@mod.route('/i/<image_id>')
+@mod.route('/i/<int:image_id>')
 def image(image_id):
-    image = Image(
-        filename="http://placekitten.com/800/600",
-        description="What a wonderfoo world.",
-        file_hash="foo",
-        upload_time=datetime.datetime(year=2013, month=2, day=12, hour=13, minute=37),
-        width=800,
-        height=600,
-        source_url="http://deviantart.com/whathuh",
-    )
-
-    artist = Artist(
-        name="KatPainter",
-    )
-
-    uploader = User(
-        name="Horsefly",
-        email="foo@foo.com",
-        password="foo",
-    )
-
+    image = Image.query.get(image_id)
+    uploader = image.user
+    artist = Artist(name="KatPainter")
 
     return render_template("booru/image.html", image=image, artist=artist, uploader=uploader)
 
